4204e86117a00a33ff293b90e7daf581880f6ed5
mdmdkamrulislam-web <md.kamrul.islam@g.bracu.ac.bd>
2022-08-20 22:26:18 +0600

    Upgraded

--- functions.py
+++ functions.py
@@ -1,66 +1,30 @@
 import alphabets as alpha
 def cipher(mode, text, shift):
-    index = 0
     temp_str = ""
-    temp_index = 0
-    for i in text:
-        for j in alpha.alphabet:
-            if(i == j):
-                index = alpha.alphabet.index(j)
-                if(mode == "encode"):
-                    if((index + shift) > 25):
-                        temp_index = (index + shift) - 26
-                        temp_str += alpha.alphabet[temp_index]
-                    else:
-                        temp_str += alpha.alphabet[index + shift]
-                if(mode == "decode"):
-                    if((index - shift) < 0):
-                        temp_index = (index - shift) + 26
-                        temp_str += alpha.alphabet[temp_index]
-                    else:
-                        temp_str += alpha.alphabet[index - shift]
-                break
-    if(mode == "encode"):
-        print(f"Your Encoded Message is: {temp_str}")
+    temp_shift = shift % 26
+    if(mode == "encode" or mode == "decode"):
+        if(mode == "decode"):
+            temp_shift *= -1
+        for char in text:
+            if(char in alpha.alphabet):
+                position = alpha.alphabet.index(char)
+                new_position = position + temp_shift
+                temp_str += alpha.alphabet[new_position]
+            else:
+                temp_str += char
+        print(f"The {mode}d text is :: {temp_str}")
     else:
-        print(f"Your Decoded Message is: {temp_str}")
-                    
-'''
-def encrypt(t, s):
-    index = 0
-    encrypted_str = ""
-    encode_index = 0
-    for i in t:
-        for j in alpha.alphabet:
-            if(i == j):
-                index = alpha.alphabet.index(j)
-                if((index + s) > 25):
-                    encode_index = (index + s) - 26
-                    encrypted_str += alpha.alphabet[encode_index]
-                else:
-                    encrypted_str += alpha.alphabet[index + s]
-                break
-    print(encrypted_str)
-
-def decrypt(t, s):
-    index = 0
-    decrypted_str = ""
-    decode_index = 0
-    for i in t:
-        for j in alpha.alphabet:
-            if(i == j):
-                index = alpha.alphabet.index(j)
-                if((index - s) < 0):
-                    decode_index = (index - s) + 26
-                    decrypted_str += alpha.alphabet[decode_index]
-                else:
-                    decrypted_str += alpha.alphabet[index - s]
-                break
-    print(decrypted_str)
-    
-def encodeOrDecode(mode, text, shift):
-    if(mode == "encode"):
-        encrypt(t = text, s = shift)
-    elif(mode == "decode"):
-        decrypt(t = text, s = shift)
-'''
+        print("Enter a Valid Mode")
+        
+def restarter():
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if(restart == "yes"):
+        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        cipher(direction, text, shift)
+        restarter()
+    elif(restart == "no"):
+        print("Thanks for using Caser Chiper.")
+    else:
+        restarter()
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import functions as fun
-
+import art
+print(art.logo)
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 fun.cipher(direction, text, shift)
+
+fun.restarter()
+
